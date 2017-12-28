5427388f6968b72ed9ee9ac3a21acbfd16cb950f
Anwesh43 <anweshthecool0@gmail.com>
2017-12-28 16:29:48 +0530

    adding HangingList

--- hanginglistview/src/main/java/ui/anwesome/com/hanginglistview/HangingListView.kt
+++ hanginglistview/src/main/java/ui/anwesome/com/hanginglistview/HangingListView.kt
@@ -37,15 +37,66 @@ class HangingListView(ctx:Context):View(ctx) {
         fun startUpdating(startcb:()->Unit) {
             dir = 1f
             scale = 0f
+            startcb()
         }
         fun execute(cb:(Float)->Unit) {
             cb(scale)
+
         }
-        fun update() {
+        fun update(stopcb: () -> Unit) {
             scale += dir*0.1f
             if(scale > 1) {
                 scale = 1f
                 dir = 0f
+                stopcb()
+            }
+        }
+    }
+    data class HangingList(var w:Float,var h:Float,var itemStrings:LinkedList<String>) {
+        val items:ConcurrentLinkedQueue<HangingItem> = ConcurrentLinkedQueue()
+        val state = HangingItemState()
+        var curr:HangingItem?=null
+        var prev:HangingItem?=null
+        init {
+            val gap = w/(2*itemStrings.size+1)
+            var x = 3*gap/2
+            var i = 0
+            itemStrings.forEach {
+                items.add(HangingItem(i,it,x,gap/2))
+                i++
+                x += 2*gap
+            }
+        }
+        fun draw(canvas:Canvas,paint:Paint) {
+            items.forEach {
+                if(it == curr) {
+                    state.execute {
+                        curr?.draw(canvas,paint,h/2,it)
+                    }
+                }
+                else if(it == prev) {
+                    state.execute {
+                        prev?.draw(canvas,paint,h/2,1-it)
+                    }
+                }
+                else {
+                    it.draw(canvas, paint, h / 2, state.scale)
+                }
+            }
+        }
+        fun update(stopcb:(Int)->Unit) {
+            state.update{
+                stopcb(curr?.i?:-1)
+                prev = curr
+            }
+        }
+        fun handleTap(x:Float,y:Float,startcb:()->Unit) {
+            items.forEach {
+                if(it != prev && it.handleTap(x,y)) {
+                    curr = it
+                    state.startUpdating(startcb)
+                    return
+                }
             }
         }
     }
